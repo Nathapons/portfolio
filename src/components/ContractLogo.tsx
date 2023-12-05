e2a3e5b1d84cc76a848c5c9fa939ddd7231f445e
nuthapon <nuthaponm79@gmail.com>
2023-12-05 19:55:11 +0700

    feat: new logo

--- src/components/ContractLogo.tsx
+++ src/components/ContractLogo.tsx
@@ -5,6 +5,7 @@ const ContractLogo: React.FC = () => {
     const linkedinUrl: string = 'https://www.linkedin.com/in/nuthapon-sripornprasert-a41138213/'
     const githubUrl: string = 'https://github.com/Nathapons'
     const facebookUrl: string = 'https://www.facebook.com/nax.seekid/?locale=th_TH'
+    const instragramUrl: string = 'https://www.instagram.com/nuthapon.s/'
 
     return (
         <div className='icon-group'>
@@ -17,7 +18,7 @@ const ContractLogo: React.FC = () => {
             {<a target="_blank" href={linkedinUrl} className="profile-style">
                 <LinkedinOutlined className="linkedin-icon" />
             </a>}
-            {<a target="_blank" href={linkedinUrl} className="profile-style">
+            {<a target="_blank" href={instragramUrl} className="instragram-style">
                 <InstagramOutlined className="linkedin-icon" />
             </a>}
         </div>
