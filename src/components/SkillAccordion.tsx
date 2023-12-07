4a7fa85bb9945c37910cb746a625563086e79e33
nuthapon <nuthaponm79@gmail.com>
2023-12-07 21:03:27 +0700

    feat: skill page

--- src/components/SkillAccordion.tsx
+++ src/components/SkillAccordion.tsx
@@ -1,34 +1,40 @@
-import { Collapse } from "antd";
+import { Collapse, Flex } from "antd";
+import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io5';
+
+import '../styles/SkillAccordion.css'
 
 const { Panel } = Collapse;
 
 interface PanelDetail {
     header: string
-    detail: string
+    icons: React.ReactNode[]
 }
 
 const SkillAccordion = () => {
     const details: PanelDetail[] = [
         {
             header: "PROGRAMMING SKILL",
-            detail: "this is defailt"
+            icons: [
+                <IoLogoHtml5 className="icon-style"/>,
+                <IoLogoCss3 className="icon-style" />,
+                <IoLogoJavascript className="icon-style"/>,
+            ]
         },
-        {
-            header: "LIBRARY AND FRAMEWORK SKILL",
-            detail: "this is defailt"
-        },
-        {
-            header: "TOOL SKILL",
-            detail: "this is defailt"
-        }
     ]
 
+    const activeKey: number[] = details.map((_, index) => index)
+
     return (
-        <Collapse>
+        <>
+            <h2>Skill</h2>
+            <Collapse defaultActiveKey={activeKey} className="collapse" bordered={true} >
             {details.map((item, index) => (
-                <Panel header={item.header} key={index}><p>{item.detail}</p></Panel>
+                <Panel key={index} header={item.header} className="item-panel">
+                    <Flex wrap="wrap" gap="small">{item.icons}</Flex>
+                </Panel>
             ))}
-        </Collapse>
+            </Collapse>
+        </>
     )
 }
 
