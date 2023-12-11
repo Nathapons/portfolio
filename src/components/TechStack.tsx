13a9ef51e1b30146db4f7ebc416aa5905fe9ee33
nuthapon <nuthaponm79@gmail.com>
2023-12-11 21:41:06 +0700

    feat: tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,14 +1,17 @@
 import React from "react"
-import myStacks from '../datas/TechStack.json'
 import { Col, Row } from "antd"
+
+import myStacks from '../datas/TechStack.json'
+import '../styles/TechStack.css'
+
 
 const TechStack: React.FC = () => {
     return (
         <>
             <h3>Tech stack</h3>
             {myStacks.map((item, index) => (
-                <Row key={index}>
-                    <Col><img src={item.src} alt={item.src} /></Col>
+                <Row key={index} className="tech-row" justify="space-between">
+                    <Col className="tech-col" ><img src={item.src} alt={item.src} className="icon-img"/><span className="text-ctn">{item.alt}</span></Col>
                 </Row>
             ))}
         </>
