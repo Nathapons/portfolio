b2957ea0250415b75bd188bc126e1d71310a22ee
nuthapon <nuthaponm79@gmail.com>
2023-12-11 22:07:21 +0700

    feat: new route

--- src/components/GithubAbout.tsx
+++ src/components/GithubAbout.tsx
@@ -3,12 +3,14 @@ import { Button } from "antd"
 import axios from "axios"
 
 import '../styles/GithubAbout.css'
+import { useNavigate } from "react-router-dom"
 
 
 const GithubAbout = () => {
     const [name, setName] = useState<string>("");
     const [createdAt, setCreatedAt] = useState<Date>(new Date());
     const [followers, setFollowers] = useState<number>(0);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const headers = {"Authorization": `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`}
@@ -22,10 +24,14 @@ const GithubAbout = () => {
         })
     }, [])
 
+    const btnClick = () => {
+        navigate('/contract')
+    }
+
     return (
         <div className="github-ctn">
             <div>
-                <Button type="primary" className="connect-btn">Connect</Button>
+                <Button type="primary" className="connect-btn" onClick={btnClick}>Connect</Button>
             </div>
             <div>
                 <h3>Nuthapon Sripornprasert</h3>
