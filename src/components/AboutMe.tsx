f35cd5762fbcc848473910fa35d8e19d3b60fdef
nuthapon <nuthaponm79@gmail.com>
2023-12-13 11:15:17 +0700

    feat: change to antd

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,17 +1,19 @@
-import '../styles/AboutMe.css'
+import { Col, Row } from 'antd'
 
 const AboutMe = () => {
     return (
-        <div className='about-me-theme'>
-            <div className='about-me-box'>
-                <div className='about-me-title'><h2>About Me</h2></div>
-                <div className='about-me-desc'>
-                    Here there! I am Nuthapon, a coding enthusiast. I love creating websites that are easy to use.
-                    I know languages like Python, Javascript, and Golang, I enjoy challenges and am constantly learning to stay on top of the latest in tech. 
-                    Whether it's fixing bugs or exploring new ideas, I'm always up for the adventure. Let's collaborate and bring your digital vision to life!
-                </div>
-            </div>
-        </div>
+        <Row style={{padding: "1.5rem 20rem", backgroundColor: "#2f5c9f", color: "white"}}>
+            <Row><Col xs={24} lg={24} style={{justifyContent: "center"}}><h2 style={{textDecoration: "underline"}}>About Me</h2></Col></Row>
+            <Row style={{fontSize: "1.2rem", textAlign: "left"}}>
+                <Col xs={24} lg={24}>
+                    <p style={{margin: "0px", fontSize: "1.2rem"}}>
+                        Here there! I am Nuthapon, a coding enthusiast. I love creating websites that are easy to use.
+                        I know languages like Python, Javascript, and Golang, I enjoy challenges and am constantly learning to stay on top of the latest in tech. 
+                        Whether it's fixing bugs or exploring new ideas, I'm always up for the adventure. Let's collaborate and bring your digital vision to life!
+                    </p>
+                </Col>
+            </Row>
+        </Row>
     )
 }
 
