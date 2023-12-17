b47c36dbad4a974d8eb66e666549be727ce35108
nuthapon <nuthaponm79@gmail.com>
2023-12-17 17:35:38 +0700

    feat: greeting

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -3,46 +3,9 @@ import Typewriter from 'typewriter-effect'
 import ContractLogo from './ContractLogo';
 
 import '../styles/Greeting.css'
-import { Col, Image, Row } from "antd";
-import styled from "styled-components";
+import { Col } from "antd";
+import { CustomImage, GreetingBox, GreetingSubBox, GreetingText, PositionText } from '../styles/Greeting';
 
-
-const CustomImage = styled(Image)`
-    width: 500px;
-    border-radius: 3.125rem;
-    animation: fadeIn 2s ease-in-out;
-
-    @keyframes fadeIn {
-        0% {opacity: 0;}
-        25% {opacity: 0.25;}
-        50% {opacity: 0.5;}
-        100% {opacity: 1;}
-    }
-`
-
-const GreetingSubBox = styled.div`
-    @media (max-width: 1050px) {
-        text-align: center;
-    }
-`
-
-const GreetingText = styled.h1`
-    font-size: 40px;
-
-    @media (max-width: 1050px) {
-        font-size: 24px;
-    }
-`
-
-const PositionText = styled.h1`
-    font-size: 2.5rem;
-    color: rgb(1, 150, 1);
-    font-style: italic;
-
-    @media (max-width: 1050px) {
-        font-size: 24px;
-    }
-`
 
  
 
@@ -60,7 +23,7 @@ const Greeting = () => {
     const img: string = "https://res.cloudinary.com/dizcg5fnc/image/upload/v1701619719/upload/kqc8rgleicctewcci0cv.jpg"
 
     return (
-        <Row gutter={[30, 30]}>
+        <GreetingBox gutter={[30, 30]}>
             <Col><CustomImage width={300} src={img} preview={false} /></Col>
             <Col>
                 <GreetingSubBox>
@@ -70,7 +33,7 @@ const Greeting = () => {
                     <ContractLogo />
                 </GreetingSubBox>
             </Col>
-        </Row>
+        </GreetingBox>
     )
 }
 
