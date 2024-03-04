dba5d367876b60b4a6c39a6b401e7234cf445b7b
Nathapons <nuthaponm79@gmail.com>
2024-03-04 23:09:38 +0700

    feat: set title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = ({ menu }: Props) => {
     return (
         <Row style={{justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#34353a', color: 'white'}}>
             <Link href="/" style={{textDecoration: "none"}}>
-                <Title level={2} style={{ margin: 0, textAlign: 'center', border: '1px solid black', padding: '2px' }}>Nuthapon.S</Title>
+                <Title level={2} style={{ margin: 0, textAlign: 'center', border: '1px solid black', padding: '5px' }}>Nuthapon.S</Title>
             </Link>
             {isMobile? <MenuBar />: <MenuButton menu={menu}/>}
         </Row>
--- components/WelcomeText.tsx
+++ components/WelcomeText.tsx
@@ -12,25 +12,33 @@ const WelcomeRow = styled(Row)`
     padding: 10px 15px;
 
     @media (min-width: 1050px) {
-        padding-top: 50px;
-        padding-bottom: 50px;
+        padding-top: 20px;
+        padding-bottom: 20px;
     }
 `
 
 const { Paragraph, Title } = Typography;
 
+const CustomParagraph = styled(Paragraph)`
+    @media (min-width: 1050px) {
+        width: 60%;
+        margin: 0 auto;
+        font-size: 18px;
+    }
+`
+
 const WelcomeText = () => {
     return (
-        <WelcomeRow gutter={[0, 10]}>
+        <WelcomeRow gutter={[0, 5]}>
             <Col span={24}>
                 <Title level={3} style={{ margin: 0, textAlign: 'center' }} underline={true}>About</Title>
             </Col>
             <Col span={24}>
-                <Paragraph>
+                <CustomParagraph>
                     I&apos;m Nuthapon, a programmer with 4 of experience. 
                     Here, you&apos;ll find a curated collection of my best work, showcasing my passion and expertise in programming. 
                     Each project represents a blend of creativity, dedication, and problem-solving skills.
-                </Paragraph>
+                </CustomParagraph>
             </Col>
         </WelcomeRow>
     )
