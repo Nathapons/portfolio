cf879c75e08dc3a1cd52a5e133276253f2a1e721
Nuthapon <nuthaponm79@gmail.com>
2025-06-04 00:54:34 +0700

    feat: update certificate

--- src/components/CertificateList.tsx
+++ src/components/CertificateList.tsx
@@ -17,7 +17,7 @@ const CertificateList: React.FC<Props> = ({ isComp }) => {
             <Row gutter={isComp ? [30, 30] : [20, 20]} justify="center" align="middle" style={{paddingBottom: '20px'}}>
                 {certificates.map((certificate, index) => (
                     <Col key={index} flex="none" xs={24} sm={24} md={24} lg={8} xl={8} xxl={8}>
-                        <Image src={certificate.url} alt={certificate.alt} width={isComp ? 500 : 350}/>
+                        <Image src={certificate.url} alt={certificate.alt} width={300}/>
                     </Col>
                 ))}
             </Row>
